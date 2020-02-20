bf7fa2f1c037d26f84adca9bd5a5a97a43088a72
Nicola <nicola.ba1989@gmail.com>
2020-02-20 16:00:44 +0100

    ok

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    // ******************************
+    $frase = "Dovresti assumere molta frutta, verdura, e fibra ogni giorno.";
+    // ******************************
+    $badwords = $_GET['badwords'];
+    // ******************************
+    $output = str_replace($badwords, '***', $frase);
+    // ******************************
+?>
+
+<!-- http://localhost:8888/php-badwords/index.php?badwords=fibra -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="dist/app.css">
+        <script src="dist/app.js" charset="utf-8"></script>
+        <title>php-badwords</title>
+    </head>
+    <body>
+        <p>
+            <?php var_dump($frase); ?>
+        </p>
+        <br>
+        <p>
+            <?php echo $output; ?>
+        </p>
+    </body>
+</html>
